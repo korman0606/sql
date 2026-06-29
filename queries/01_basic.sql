74e5457142418344590966493892a9bb4862085b
korman0606 <kormanv50@gmail.com>
2026-06-29 18:07:02 +0300

    Update 01_basic.sql

--- queries/01_basic.sql
+++ queries/01_basic.sql
@@ -1,3 +1,7 @@
 -- Task 1. How many customers are in the database?
 SELECT count(distinct customer_unique_id)
 FROM customers;
+
+-- Task 2. How many orders have been placed?
+SELECT count(*)
+FROM orders;

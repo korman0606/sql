a9288c654935c6c91f789aa4aff24b303808f481
korman0606 <kormanv50@gmail.com>
2026-06-29 18:11:32 +0300

    Update 01_basic.sql

--- queries/01_basic.sql
+++ queries/01_basic.sql
@@ -5,3 +5,7 @@ FROM customers;
 -- Task 2. How many orders have been placed?
 SELECT count(*)
 FROM orders;
+
+-- Task 3. How many sellers are registered?
+SELECT COUNT(*) AS sellers_count
+FROM sellers;

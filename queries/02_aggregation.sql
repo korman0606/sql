79cf14bf50d8a510b369b8b3322b23371008719d
korman0606 <kormanv50@gmail.com>
2026-06-29 23:20:36 +0300

    Update 02_aggregation.sql

--- queries/02_aggregation.sql
+++ queries/02_aggregation.sql
@@ -1,6 +1,15 @@
 -- Task 21. How many orders were placed for each order status?
-SELECT order_status, COUNT(*) AS orders_count
+SELECT 
+  order_status, 
+  COUNT(*) AS orders_count
 FROM orders
 GROUP BY order_status
 ORDER BY orders_count DESC;
 
+-- Task 22. How many customers are there in each state?
+SELECT 
+  customer_state, 
+  COUNT(customer_unique_id) AS customers_count
+FROM customers
+GROUP BY customer_state
+ORDER BY customers_count DESC;

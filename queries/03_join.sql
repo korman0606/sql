f3a873c95ed49481171c95d629c284cb6f7062e0
korman0606 <kormanv50@gmail.com>
2026-06-30 23:27:44 +0300

    Update 03_join.sql

--- queries/03_join.sql
+++ queries/03_join.sql
@@ -9,3 +9,15 @@ GROUP BY c.customer_unique_id
 ORDER BY orders_count DESC
 LIMIT 10;
 
+-- Task 37. Top 10 product categories by revenue.
+SELECT
+	p.product_category_name,
+	SUM(o.price) AS total_revenue
+FROM products p
+INNER JOIN order_items o
+ON p.product_id = o.product_id
+GROUP BY p.product_category_name
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+

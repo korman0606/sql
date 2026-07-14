5f229c062ce79d5aa18480a441f99a183f80316e
korman0606 <kormanv50@gmail.com>
2026-07-15 01:07:20 +0500

    Update 04_window_functions.sql

--- queries/04_window_functions.sql
+++ queries/04_window_functions.sql
@@ -6,3 +6,15 @@ SELECT
 FROM order_items
 GROUP BY seller_id
 LIMIT 10;
+
+-- Task 52. Dense rank product categories by revenue (top 10).
+SELECT
+	p.product_category_name,
+	SUM(oi.price) AS revenue,
+	DENSE_RANK() OVER (ORDER BY SUM(oi.price) DESC) AS revenue_rank
+FROM order_items oi
+	INNER JOIN products p
+	ON oi.product_id = p.product_id
+GROUP BY p.product_category_name
+ORDER BY revenue_rank
+LIMIT 10;
